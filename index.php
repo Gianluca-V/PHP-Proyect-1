dae2ce41f681c0baedea7edb45acd85721dfb582
Gianluca-V <107764977+Gianluca-V@users.noreply.github.com>
2023-05-19 18:38:43 -0300

    first commit
    first commit of the proyect

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    
+    
+    <div class="container">
+        <h1>Escuela</h1>
+        <a href="FormularioCargarAlumno.php" class="cargarAlumno container-link"> 
+            <div>
+                Cargar Alumno 
+            </div>
+        </a>
+        
+
+        <a href="FormularioMostrarAlumno.php" class="cargarAlumno container-link"> 
+            <div>
+                Mostrar Alumno 
+            </div>
+        </a>
+
+        <a href="SolicitudDeCurso.php" class="cargarAlumno container-link"> 
+            <div>
+                Mostrar Mejor Alumno De curso
+            </div>
+        </a>
+    </div>
+
+</body>
+</html>
