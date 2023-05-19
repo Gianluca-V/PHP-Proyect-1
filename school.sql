93a9842a38bd0776e6f66a279a30fe6bc90ac2a5
Gianluca-V <107764977+Gianluca-V@users.noreply.github.com>
2023-05-19 20:42:36 -0300

    Database created

---
+++ school.sql
@@ -0,0 +1,168 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 20-05-2023 a las 01:41:20
+-- Versión del servidor: 10.1.21-MariaDB
+-- Versión de PHP: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `school`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumn`
+--
+
+CREATE TABLE `alumn` (
+  `dni` varchar(9) NOT NULL,
+  `name` varchar(128) DEFAULT NULL,
+  `surname` varchar(128) DEFAULT NULL,
+  `average` double DEFAULT '0',
+  `image` varchar(2048) DEFAULT NULL,
+  `course` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Disparadores `alumn`
+--
+DELIMITER $$
+CREATE TRIGGER `update_average_trigger` BEFORE INSERT ON `alumn` FOR EACH ROW BEGIN
+    SET NEW.average = calculate_average(NEW.dni);
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `update_average_trigger2` BEFORE UPDATE ON `alumn` FOR EACH ROW BEGIN
+    SET NEW.average = calculate_average(NEW.dni);
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `course`
+--
+
+CREATE TABLE `course` (
+  `id` int(11) NOT NULL,
+  `year` int(11) DEFAULT NULL,
+  `division` varchar(3) DEFAULT NULL,
+  `speciality` varchar(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `notes`
+--
+
+CREATE TABLE `notes` (
+  `id` int(11) NOT NULL,
+  `subjectId` int(11) DEFAULT NULL,
+  `alumnId` varchar(128) DEFAULT NULL,
+  `note` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `subject`
+--
+
+CREATE TABLE `subject` (
+  `id` int(11) NOT NULL,
+  `name` varchar(128) DEFAULT NULL,
+  `course` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `alumn`
+--
+ALTER TABLE `alumn`
+  ADD PRIMARY KEY (`dni`),
+  ADD KEY `course` (`course`);
+
+--
+-- Indices de la tabla `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `notes`
+--
+ALTER TABLE `notes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `subjectId` (`subjectId`),
+  ADD KEY `alumnId` (`alumnId`);
+
+--
+-- Indices de la tabla `subject`
+--
+ALTER TABLE `subject`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `course` (`course`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `course`
+--
+ALTER TABLE `course`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `notes`
+--
+ALTER TABLE `notes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `subject`
+--
+ALTER TABLE `subject`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `alumn`
+--
+ALTER TABLE `alumn`
+  ADD CONSTRAINT `alumn_ibfk_1` FOREIGN KEY (`course`) REFERENCES `course` (`id`);
+
+--
+-- Filtros para la tabla `notes`
+--
+ALTER TABLE `notes`
+  ADD CONSTRAINT `notes_ibfk_1` FOREIGN KEY (`subjectId`) REFERENCES `subject` (`id`),
+  ADD CONSTRAINT `notes_ibfk_2` FOREIGN KEY (`alumnId`) REFERENCES `alumn` (`dni`);
+
+--
+-- Filtros para la tabla `subject`
+--
+ALTER TABLE `subject`
+  ADD CONSTRAINT `subject_ibfk_1` FOREIGN KEY (`course`) REFERENCES `course` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
