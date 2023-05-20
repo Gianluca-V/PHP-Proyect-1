24dd903538b1a5fa63c3a598c0572703fbe6a7e5
Gianluca-V <107764977+Gianluca-V@users.noreply.github.com>
2023-05-19 22:13:57 -0300

    Add files via upload

--- FormularioCargarAlumno.php
+++ FormularioCargarAlumno.php
@@ -7,20 +7,12 @@
     <title>Campus</title>
 </head>
 <body>
-    <form method="POST">
-        <input type="submit" value="Cargar alumno" name="enviar">
-    </form>
 
-    <?php
-
-    if(isset($_POST['enviar'])){
-        $enviar = $_POST['enviar'];
-        
-        echo '<form method="POST">';
-        echo '<input type="text" placeholder="Nombre" name="nombre">';
-        echo '<input type="text" placeholder="Apellido" name="apellido">';
-        echo '<input type="text" placeholder="DNI" name="dni">';
-        echo '<select name="curso">
+       <form method="POST">
+        <input type="text" placeholder="Nombre" name="nombre">
+        <input type="text" placeholder="Apellido" name="apellido">
+        <input type="text" placeholder="DNI" name="dni">
+        <select name="curso">
 
                 <option>1ro</option>
 
@@ -36,40 +28,48 @@
 
                 <option>7mo</option>
 
-            </select>';
+            </select>
 
-        echo '<input type="text" placeholder="Imágen del alumno (link)" name="imagen">';
+        <input type="text" placeholder="Imágen del alumno (link)" name="imagen">
 
-        echo '<input type="submit" value="Enviar" name="enviarForm">';
+        <input type="submit" value="Enviar" name="enviarForm">
 
-        echo '</form>';
+        </form>
+
+        <?php
 
         include "conexion.php";
-        if(isset($_POST['enviarForm'])){
-            $enviarForm = $_POST['enviarForm'];
+        
 
-            $nombre=$_POST['nombre'];
-            $apellido=$_POST['apellido'];
-            $dni=$_POST['dni'];
-            $curso=$_POST['curso'];
-            $imagen=$_POST['imagen'];
+        if(isset($_POST['enviarForm'])){         
+            echo "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+            $nombre = mysqli_real_escape_string($conexion, $_POST['nombre']);
+            $apellido = mysqli_real_escape_string($conexion, $_POST['apellido']);
+            $dni = mysqli_real_escape_string($conexion, $_POST['dni']);
+            $curso = mysqli_real_escape_string($conexion, $_POST['curso']);
+            $imagen = mysqli_real_escape_string($conexion, $_POST['imagen']);
 
             $query_consulta=mysqli_query($conexion, "SELECT * FROM alumn WHERE
             dni='$dni'");
             $fila=mysqli_num_rows($query_consulta);
             if($fila == 1){ 
-
-            echo 'El usuario ya existe. Intente nuevamente';
+                mysqli_error($conexion);
+                echo 'El usuario ya existe. Intente nuevamente';
             }
             else{
                 $query_consulta=mysqli_query($conexion, "INSERT INTO alumn (name, surname, dni, course, image)
-                                     VALUES('$nombre', '$apellido', '$dni', '$curso', '$imagen')");
-                                     mysqli_error($conexion);
-            echo 'Se logró el registro';
+                                VALUES('$nombre', '$apellido', '$dni', '$curso', '$imagen')");
+
+                
+
+                if(!$query_consulta){
+                    echo 'Error en el registro: ' . mysqli_error($conexion);
+                } else {
+                    echo 'Se logró el registro';
+                }
             }
 
         }
-    }
 
     ?>
 </body>
